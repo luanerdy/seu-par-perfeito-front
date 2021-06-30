09e2257f2a69bde5578286a84d7cb7fd91942c43
luanerdy <luan.vini1@gmail.com>
2021-06-30 17:29:32 -0300

    adjust

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { SignUpPage } from './pages/SignUpPage';
 import { LogInPage } from './pages/LogInPage';
-import Store from './components/Store/Store';
+import Store from './components/Store/Store.js';
 import Resetcss from './Resetcss.js';
 
 function App() {
